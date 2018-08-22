bc7a4fd9a726dbc4c50aa6f992c57b415ef3c0f0
rimadoma <richard.domander@gmail.com>
2018-08-22 14:54:26 +0100

    Add Travis configuration

---
+++ .travis/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-javadoc.sh
+sh travis-javadoc.sh BoneJ $encrypted_0ed02f82cadf_key $encrypted_0ed02f82cadf_iv
